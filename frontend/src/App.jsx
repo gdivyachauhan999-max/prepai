817f5f89cf8993cd6b98f826006ec335f0a9e599
Priyanshu Sharma <sharmapriyanshu8488@gmail.com>
2026-07-26 15:30:14 -0500

    fix frontend issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Footer from "./components/Footer";
@@ -17,7 +17,8 @@ function App() {
       <BrowserRouter>
         <>
           <Routes>
-            <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route
               path="/dashboard"
